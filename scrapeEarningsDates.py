6451775440b44f6614a2bc3f8c466221aad2b799
ack8006 <takata.alex@gmail.com>
2015-10-13 12:05:14 -0400

    first commit

---
+++ scrapeEarningsDates.py
@@ -0,0 +1,70 @@
+from lxml import html
+import requests
+from dbconnection import start_db_connection
+from contextlib import closing
+import datetime
+import psycopg2
+
+
+DB_CONNECTION_TYPE = 'local'
+
+
+def scrape_earnings_page(last_date):
+    string_date = calculate_string_date(last_date)
+    url = 'http://biz.yahoo.com/research/earncal/{}.html'.format(string_date)
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+    earnings_lines_count = len(tree.xpath('//*/td/table[1]/tr'))
+    print earnings_lines_count
+    lines = []
+    for x in xrange(3, earnings_lines_count):
+        line = tree.xpath('//*/td/table[1]/tr[{}]/td//text()'.format(str(x)))
+        if len(line) == 1: continue
+        lines.append(prepare_line(line))
+    upload_earnings_information(last_date, lines)
+
+def prepare_line(line):
+    #If no ticker
+    if len(line) == 2: line.append(None)
+    elif line[2] == 'Time Not Supplied': line[2] = None
+    #Sometimes unndeeded fourth entry
+    return line[:3]
+
+def upload_earnings_information(date, lines):
+    conn = start_db_connection(DB_CONNECTION_TYPE)
+    with closing(conn.cursor()) as cur:
+        cur.execute('''SELECT ticker FROM cusiplist''')
+        tickers = [x[0] for x in cur.fetchall()]
+        for line in lines:
+            name, ticker, time = line
+            if ticker in tickers:
+                try:
+                    cur.execute('''INSERT INTO equityearningsdates
+                                (ticker, earningsdate, time) VALUES
+                                (%s,%s,%s)''',(ticker, date, time))
+                except psycopg2.IntegrityError:
+                    conn.rollback()
+    conn.commit()
+    conn.close()
+
+
+def calculate_string_date(last_date):
+    return last_date.strftime("%Y%m%d")
+
+def get_min_db_date():
+    conn = start_db_connection(DB_CONNECTION_TYPE)
+    with closing(conn.cursor()) as cur:
+        cur.execute('SELECT MAX(earningsdate) FROM equityearningsdates')
+        last_date = cur.fetchone()[0]
+    conn.close()
+    return last_date
+
+
+if __name__ == '__main__':
+    last_date = get_min_db_date()
+    to_date = datetime.datetime.now().date()+datetime.timedelta(days=14)
+    print last_date, to_date
+    while last_date < to_date:
+        scrape_earnings_page(last_date)
+        last_date += datetime.timedelta(days=1)
+
